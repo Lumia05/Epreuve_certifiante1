553111dd354f21168b7fd70b7630088ab95ff8bb
Lumia05 <suzielumiere07@gamil.com>
2026-02-12 20:59:31 +0100

    deuxième commit

---
+++ src/main/java/Epreuve_Certifiante/EpreuveCertifianteApplication.java
@@ -0,0 +1,13 @@
+package Epreuve_Certifiante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpreuveCertifianteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpreuveCertifianteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Epreuve_Certifiante/EpreuveCertifianteApplicationTests.java
@@ -0,0 +1,13 @@
+package Epreuve_Certifiante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpreuveCertifianteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
